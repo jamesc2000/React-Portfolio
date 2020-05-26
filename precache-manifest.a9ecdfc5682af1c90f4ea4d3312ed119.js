6879a995149ba20154a52a06dfa0fe9b3b24c64d
jamesc2000 <cruz.james99@gmail.com>
2020-05-26 17:48:56 +0800

    Updates

---
+++ precache-manifest.a9ecdfc5682af1c90f4ea4d3312ed119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dcd26a037442ddcc3ab7a63da4711b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "46285802a24c41fb85ba",
+    "url": "/React-Portfolio/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "51858334b37a6e4d6359",
+    "url": "/React-Portfolio/static/js/2.f57be14e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.f57be14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46285802a24c41fb85ba",
+    "url": "/React-Portfolio/static/js/main.e46a7a98.chunk.js"
+  },
+  {
+    "revision": "4918349469a6b19a6422",
+    "url": "/React-Portfolio/static/js/runtime-main.19999322.js"
+  }
+]);
